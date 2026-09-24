9b03c2ac29ad442a52a76e0629a7a7bd47eadaa8
benedicbl <lesmanabenny89@gmail.com>
2026-09-24 11:39:29 +0700

    delete unnecesarry stuff and adding new stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+    <h1 class="text-5xl font-extrabold text-blue-600 mb-4">
+      Halo, Tailwind! 👋
+    </h1>
+    <p class="text-lg text-red-700 font-medium px-6 py-3 bg-white shadow-md rounded-lg border border-gray-200">
+      Ini adalah halaman testing pertama dari HomeView.vue
+    </p>
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js" // Tambahkan ini karena sebelumnya Anda menginstal Flowbite
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin') // Jangan lupa tambahkan plugin Flowbite jika ingin langsung dipakai
+  ],
 }
-
